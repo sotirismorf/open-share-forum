cc36e920d2aba980e7171a6d8b0b63e97a9f4c29
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-01-28 04:14:34 +0200

    login page improvements

--- website/src/routes/login.svelte
+++ website/src/routes/login.svelte
@@ -1,12 +1,17 @@
 <script>
+	import { goto } from '$app/navigation';
 	import Card from '$lib/Card.svelte';
 	import ButtonPrimary from '$lib/ButtonPrimary.svelte';
+	import InputPrimary from '$lib/InputPrimary.svelte';
 
 	import { store } from '$lib/utils/auth';
 
 	let username = '';
+	let password = '';
+	let error = '';
 
 	async function onClickLogInButton() {
+		console.log(password);
 		const response = await fetch('http://localhost:4000/auth/signin', {
 			method: 'POST', // *GET, POST, PUT, DELETE, etc.
 			//mode: 'cors', // no-cors, *cors, same-origin
@@ -18,7 +23,10 @@
 			},
 			//redirect: 'follow', // manual, *follow, error
 			//referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-			body: JSON.stringify({ username: username}) // body data type must match "Content-Type" header
+			body: JSON.stringify({
+				username: username,
+				password: password
+			}) // body data type must match "Content-Type" header
 		});
 
 		if (response.ok) {
@@ -28,16 +36,27 @@
 				id: data.id,
 				token: data.token
 			};
+			goto('/');
 		} else {
-			console.log('fail')
+			const data = await response.json();
+			console.log(data.message);
+			error = data.message;
 		}
-
 	}
 </script>
 
-<title>THMMY: Log In</title>
+<title>THMMY: Sign In</title>
 <Card>
-	<h1>Login</h1>
-	<input class="bg-slate-600" bind:value={username} placeholder="Username" />
-	<ButtonPrimary title="Log In" onClick={onClickLogInButton} />
+	<h1 class="text-3xl font-bold text-center pb-7">Συνδέσου στο ΤΗΜΜΥ</h1>
+	<form class="flex flex-col items-center gap-3" on:submit|preventDefault={onClickLogInButton}>
+		<InputPrimary bind:value={username} description="Username" placeholder="Username" type="text" />
+		<InputPrimary
+			bind:value={password}
+			description="Password"
+			placeholder="Password"
+			type="password"
+		/>
+		<ButtonPrimary title="Sign In" type="submit" />
+	</form>
+	<p>{error}</p>
 </Card>
