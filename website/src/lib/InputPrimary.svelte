01c7a2451b4316aa475baabfd8ac8850fafce3fc
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-01-28 04:12:10 +0200

    new Input Component

---
+++ website/src/lib/InputPrimary.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let placeholder;
+	export let type;
+	export let description;
+	export let value;
+
+	const setType = (node) => {
+	  node.type = type;
+	};
+
+</script>
+
+<div>
+	<p class="pl-5 pb-1 text-xs font-bold">{description}</p>
+	<input
+		class="bg-slate-600 rounded-full py-2 px-4 max-w-xs transition-colors focus:bg-slate-500"
+		{placeholder}
+		use:setType
+		bind:value
+	/>
+</div>
