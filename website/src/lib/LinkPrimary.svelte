ea052d1338dacc4b27ae1f845c9ed1674527e761
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-01-29 04:10:00 +0200

    new Link Component (same style as ButtonPrimary)

---
+++ website/src/lib/LinkPrimary.svelte
@@ -0,0 +1,13 @@
+<script>
+	//export let title;
+	export let href;
+</script>
+
+<a
+	href={href}
+	class="font-bold rounded-full px-3 py-1 w-min h-min whitespace-nowrap
+		bg-green-700 sm:hover:bg-green-600 active:bg-green-600 
+		outline-none"
+>
+	<slot></slot>
+</a>
