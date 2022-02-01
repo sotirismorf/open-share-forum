1d23c4620817c371bc60f5192685b1a557887e4d
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-02-01 03:51:58 +0200

    controller imports

--- server/src/index.js
+++ server/src/index.js
@@ -3,45 +3,26 @@ const cors = require('cors')
 const pool = require('./db')
 const controller = require('./controllers/authController')
 const postController = require('./controllers/postController')
+const downloadsController = require('./controllers/downloadsController')
+const courseController = require('./controllers/courseController')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+app.use('/downloads/preview', express.static('/home/sotiris/testing'))
+
+app.get('/downloads/get/:file', (req, res) => {
+  const file = `/home/sotiris/testing/10.pdf`;
+  res.download(file); // Set disposition and send it.
+});
+
 app.get('/', async (_req, res) => {
   try {
     res.send('Welcome to the THMMY API')
   } catch (err) {
     console.error(err.message)
-  }
-})
-
-app.get('/users', async (_req, res) => {
-  try {
-    const users = await pool.query('SELECT * FROM users')
-    res.json(users.rows)
-  } catch (err) {
-    console.error(err.message)
-  }
-})
-
-app.post('/users', async (req, res) => {
-  try {
-    const { username, name } = req.body
-
-    const posts = await pool.query(
-      '\
-      INSERT INTO users (id,username,name) \
-      VALUES (DEFAULT,$1,$2) \
-      RETURNING *',
-      [username, name]
-    )
-
-    res.json(posts.rows[0])
-  } catch (err) {
-    console.error(err.message)
-    res.status(400).send()
   }
 })
 
@@ -80,10 +61,14 @@ app.get('/users/get/:username', async (req, res) => {
 app.get('/posts', postController.getPosts)
 app.get('/posts/:id', postController.getPostbyID)
 app.get('/posts/:id/comments', postController.getPostComments)
-
 app.post('/posts', postController.createPost)
 
 app.get('/users/:id/posts', postController.getUserPosts)
+
+app.get('/downloads/page/:page', downloadsController.getDownloads)
+app.post('/downloads', downloadsController.newUpload)
+
+app.get('/courses', courseController.getCourses)
 
 app.post('/auth/signin', controller.signin)
 app.post('/auth/signup', controller.signup)
