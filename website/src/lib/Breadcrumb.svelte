38389ba0c25f39033f65952ffb8f94e7dbfe136a
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-02-04 04:31:51 +0200

    Added Breadcrumb tomponent

---
+++ website/src/lib/Breadcrumb.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let items;
+</script>
+
+<h1 class="text-xl font-bold">
+	<a href="/downloads"> Downloads </a>
+	{#each items as item}
+		<span>/</span>
+		<a class="hover:text-gray-400" href="/downloads/{item.href}">
+			{item.title}
+		</a>
+	{/each}
+</h1>
