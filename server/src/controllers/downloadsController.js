d15cfb6f36d899b959566c08efc4cf46acef89ac
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-02-16 02:39:39 +0200

    working file upload

--- server/src/controllers/downloadsController.js
+++ server/src/controllers/downloadsController.js
@@ -15,7 +15,6 @@ exports.getDownloads = async (req, res) => {
         .send({ message: `Error: No course with name ${course}` })
     }
 
-    console.log(courseID.rows[0].id)
     const data = await pool.query('SELECT * FROM files WHERE course=$1', [
       courseID.rows[0].id,
     ])
@@ -69,32 +68,44 @@ exports.newUpload = async (req, res) => {
     isExam,
     examSemester,
     hasSolutions,
-  } = req.body
-
+  } = JSON.parse(req.body.document)
 
   try {
-    const data = await pool.query(
-      'INSERT INTO files \
-      (id,author,name,filetype,description,detail,course,year,is_exam,exam_semester,has_solutions) \
-      VALUES (default,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10) \
-      RETURNING *',
-      [
-        author,
-        filename,
-        filetype,
-        description,
-        detail,
-        course,
-        year,
-        isExam,
-        examSemester,
-        hasSolutions,
-      ]
-    )
+    if (!req.files) {
+      res.send({
+        //status: false,
+        message: 'No file uploaded',
+      })
+    } else {
+      const fileData = await pool.query(
+        'INSERT INTO files \
+        (id,author,name,filetype,description,detail,course,year,is_exam,exam_semester,has_solutions) \
+        VALUES (default,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10) \
+        RETURNING *',
+        [
+          author,
+          filename,
+          filetype,
+          description,
+          detail,
+          course,
+          year,
+          isExam,
+          examSemester,
+          hasSolutions,
+        ]
+      )
 
-    res.status(200).json(data.rows)
+      let file = req.files.file;
+
+      console.log(fileData.rows[0].id)
+      file.mv(`/home/sotiris/thmmyrepository/${fileData.rows[0].id}.${filetype}`)
+      res.send({ message: 'File Uploaded' })
+    }
   } catch (err) {
+    //res.status(200).json(data.rows)
     console.error(err.message)
     res.status(501).send({ message: err.message })
+    //res.status(501).sendStatus({ message: err.message })
   }
 }
